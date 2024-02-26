adb153df19eb9c0285f3871f2f3a428379d94358
subham8391 <subhamdas967990@gmail.com>
2024-02-26 13:37:56 +0530

    update commit

--- src/Header/SearchResults.jsx
+++ src/Header/SearchResults.jsx
@@ -1,5 +1,5 @@
-import React, { useEffect, useState, useRef } from 'react';
-import { useLocation } from 'react-router-dom';
+import React, { useEffect, useState } from 'react';
+import { useLocation} from 'react-router-dom';
 
 function SearchResults() {
     const location = useLocation();
@@ -7,28 +7,25 @@ function SearchResults() {
     const [searchResults, setSearchResults] = useState([]);
     const [page, setPage] = useState(1);
     const [totalPages, setTotalPages] = useState(1);
-    const [loading, setLoading] = useState(false);
-    const bottomBoundaryRef = useRef(null);
+    const [loading, setLoading] = useState(true);
 
     useEffect(() => {
         const fetchSearchResults = async () => {
-            if (!loading && page <= totalPages) {
+            try {
                 setLoading(true);
-                try {
-                    const apiKey = '6f6546c488597640b6c611e630aca586';
-                    const url = `https://api.themoviedb.org/3/search/movie?page=${page}&api_key=${apiKey}&query=${query}`;
-                    const response = await fetch(url);
-                    if (!response.ok) {
-                        throw new Error('Network response was not ok');
-                    }
-                    const data = await response.json();
-                    setSearchResults(prevContent => [...prevContent, ...data.results]);
-                    setTotalPages(data.total_pages);
-                    setPage(prevPage => prevPage + 1);
-                } catch (error) {
-                    console.error('Error Fetching Data:', error);
-                    setSearchResults([]);
+                const apiKey = '6f6546c488597640b6c611e630aca586';
+                const url = `https://api.themoviedb.org/3/search/movie?page=${page}&api_key=${apiKey}&query=${query}`;
+                const response = await fetch(url);
+                if (!response.ok) {
+                    throw new Error('Network response was not ok');
                 }
+                const data = await response.json();
+                setSearchResults(prevResults => [...prevResults, ...data.results]);
+                setTotalPages(data.total_pages);
+            } catch (error) {
+                console.error('Error Fetching Data:', error);
+                setSearchResults([]);
+            } finally {
                 setLoading(false);
             }
         };
@@ -36,49 +33,43 @@ function SearchResults() {
         if (query && query !== '') {
             fetchSearchResults();
         }
+    }, [query, page]);
 
-        const handleScroll = () => {
-            if (
-                bottomBoundaryRef.current &&
-                bottomBoundaryRef.current.getBoundingClientRect().top <= window.innerHeight
-            ) {
-                fetchSearchResults();
-            }
-        };
-
-        window.addEventListener('scroll', handleScroll);
-        window.scrollTo(0, 0); // Scroll to top when the component mounts
-
-        return () => {
-            window.removeEventListener('scroll', handleScroll);
-        };
-    }, [query, loading, page, totalPages]);
+    const handleLoadMore = () => {
+        setPage(prevPage => prevPage + 1);
+    };
 
     return (
-        <div className="search-results">
-            <div className="search-result-section">
-                {searchResults.map((item) => (
-                    <div className="movie-container" key={item.id}>
-                        <div className="movie-card">
-                            <div className="movie-img">
-                                <img src={`https://image.tmdb.org/t/p/w500${item.poster_path}`} alt="" />
-                            </div>
-                            <div className="movie-details">
-                                <div className="m-detail">
-                                    <p>{item.title}</p>
-                                    <p>{item.vote_average.toFixed(1)}</p>
+        <>
+            <div className="search-results">
+                <div className="search-result-section">
+                    {searchResults.map((item) => (
+                        <div className="movie-container" key={item.id}>
+                            <div className="movie-card">
+                                <div className="movie-img">
+                                    <img src={`https://image.tmdb.org/t/p/w500${item.poster_path}`} alt="" />
                                 </div>
-                                <div className="m-detail">
-                                    <p>Language: {item.original_language}</p>
-                                    <p>{item.release_date}</p>
+                                <div className="movie-details">
+                                    <div className="m-detail">
+                                        <p>{item.title}</p>
+                                        <p>{item.vote_average.toFixed(1)}</p>
+                                    </div>
+                                    <div className="m-detail">
+                                        <p>Language: {item.original_language}</p>
+                                        <p>{item.release_date}</p>
+                                    </div>
                                 </div>
                             </div>
                         </div>
+                    ))}
+                </div>
+                {!loading && totalPages > page && (
+                    <div className="load-more">
+                        <button onClick={handleLoadMore}>Load More</button>
                     </div>
-                ))}
-                <div ref={bottomBoundaryRef}></div>
+                )}
             </div>
-        </div>
+        </>
     );
 }
 
